c7f9476d3478486f38bfe0fb54bef656d6e71c3d
JavierM <144504968+jav0314@users.noreply.github.com>
2024-06-18 19:10:17 -0500

    connection with Back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './lib/authenticationFields/log-in/log-in.component';
@@ -14,7 +14,7 @@ import { SlideBarComponent } from './lib/slide-bar/slide-bar.component';
     SingUpComponent,
     SlideBarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/lib/interface/userDetail.interface.ts
@@ -0,0 +1,13 @@
+export interface Users {
+  Cod: string;
+  Id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  dateBirth: string;
+  Email: string;
+  Phone: string;
+  userName: string;
+  Password: string;
+  Nationality: string;
+}
